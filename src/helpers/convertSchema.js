f7b9358afddbffbd33beb7d21348061ac2218382
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-04-16 14:45:17 -0400

    changes to convertSchema and unit.js

--- src/helpers/convertSchema.js
+++ src/helpers/convertSchema.js
@@ -3,23 +3,27 @@ const Sequelize = require('sequelize');
 //using the .models method, traverses down to the attributes and stores all
 //attribute names in an array along with the associated table
 
-const hasRelation = (attr) => {
-    if (attr['references']) return true;
-    return false;
-}
+// const hasRelation = (attr) => {
+//     if (attr['references']) return true;
+//     return false;
+// }
 
 const convertSchemas = (schema) => {
-    let schemas = {};
+    let schemas = [];
     for (let modelKey in schema.models) {
         const model = schema.models[modelKey];
         let columnNames = [];
         for (let attribute in model.rawAttributes) {
             let dataType = model.rawAttributes[attribute]['type'].constructor.key;
-            columnNames.push({ content: attribute, type: dataType, relation: hasRelation(model.rawAttributes[attribute]) });
+            columnNames.push({ content: attribute, type: dataType, relation: model.rawAttributes[attribute]['references'] });
         }
 
-        schemas[modelKey] = columnNames;
+        schemas.push({
+            name: modelKey,
+            columns: columnNames, 
+        });
     }
+
     return schemas;
 }
 
